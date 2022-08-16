834f6483515e2d0e4dc882b3dbb8f57d59b0ea37
BENGOBRO <sponkeylox6@gmail.com>
2022-08-16 17:23:38 +0800

    Added functionality: connect and login into the server; download and upload files

--- src/main/java/org/infotecs/FTPServer.java
+++ src/main/java/org/infotecs/FTPServer.java
@@ -1,27 +1,105 @@
 package org.infotecs;
 
-import java.net.*;
+import java.io.*;
+import org.apache.commons.net.ftp.FTPClient;
 
 public class FTPServer {
 
-    private String host = "";
-    private final int PORT = 8888;
+    private static final int PORT = 21;
+    private String IP;
+    private final FTPClient ftp = new FTPClient();
+    private boolean isPassive;
 
-    private String username;
-    private String password;
-    private Socket client;
-    //private JsonParser jsonParser;
-
-    public void connect() {
-
+    public boolean connect() {
         try {
-            //ftp = new FTPClient();
+            setMode();
+            ftp.connect(IP, PORT);
+            showServerReply();
         } catch(Exception e) {
-            System.out.println("Error: can't connect to server");
+            if (ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                }
+                catch (final IOException f) {
+                }
+            }
+            System.out.printf("Failed to connect to the server! Error: %s\n", e.toString());
+        } finally {
+            return ftp.isConnected();
         }
     }
 
-    public void setHost (String host) {
-        this.host = host;
+    private void setMode() {
+        if (isPassive) {
+            ftp.enterLocalPassiveMode();
+        } else {
+            ftp.enterLocalActiveMode();
+        }
+    }
+
+    public boolean signIn(String username, String password) {
+        setMode();
+        boolean isLogged = false;
+        try {
+            isLogged = ftp.login(username, password);
+        } catch (IOException e) {
+            System.out.printf("Error: %s\n", e.toString());
+        }
+        return isLogged;
+    }
+
+    public boolean downloadFile(String remoteFileName, String localFileName) {
+        boolean isDownloaded = false;
+        setMode();
+        ftp.setControlKeepAliveTimeout(300);
+        try (OutputStream output = new FileOutputStream(localFileName)) {
+            isDownloaded = ftp.retrieveFile(remoteFileName, output);
+            System.out.println("Download successful!");
+        } catch (IOException e) {
+            System.out.printf("Error: %s\n", e.toString());
+        }
+        return isDownloaded;
+    }
+
+    public void uploadFile(String remoteFileName, String localFileName) {
+        try {
+            ftp.deleteFile(remoteFileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        setMode();
+        try (InputStream input = new FileInputStream(localFileName)) {
+            ftp.appendFile(remoteFileName, input);
+            System.out.println("Upload successful!");
+        } catch (IOException e) {
+            System.out.printf("Error: %s\n", e.toString());
+        }
+    }
+
+    public void disconnect() {
+        try {
+            ftp.disconnect();
+            System.out.println("Connection disabled");
+        } catch (IOException e) {
+            System.out.print("Error: ");
+            e.printStackTrace();
+        }
+    }
+
+    public void setPassive(boolean passive) {
+        isPassive = passive;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+    private void showServerReply() {
+        String[] replies = ftp.getReplyStrings();
+        if (replies != null && replies.length > 0) {
+            for (String aReply : replies) {
+                System.out.println("SERVER: " + aReply);
+            }
+        }
     }
 }
